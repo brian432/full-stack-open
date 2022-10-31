ec05c1fcb39cb881f9a75b755584e00fbef61f28
Brian Cabrera <80001602+brian432@users.noreply.github.com>
2022-10-31 20:09:09 -0300

    . (#2)

--- models/User.js
+++ models/User.js
@@ -4,16 +4,23 @@ const uniqueValidator = require('mongoose-unique-validator')
 const userSchema = new Schema({
   username: {
     type: String,
+    required: true,
     unique: true
   },
-  name: String,
-  passwordHash: String,
-  notes: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Note'
-    }
-  ]
+  name: {
+    type: String,
+    required: true
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+    notes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Note'
+      }
+    ]
+  }
 })
 
 userSchema.set('toJSON', {
